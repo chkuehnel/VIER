a8b3a4cf7fb8470db1b4f6ce971cb4fa795bed8b
ch.kuehnel <ch.kuehnel@gmx.de>
2014-10-22 18:39:53 +0200

    Unused code deleted, init() and addListener method added.

--- app/src/main/java/com/bu/haw/vier/StartActivity.java
+++ app/src/main/java/com/bu/haw/vier/StartActivity.java
@@ -1,19 +1,17 @@
 package com.bu.haw.vier;
 
+import com.bu.haw.vier.data.DataStorage;
+import com.bu.haw.vier.data.ExStorageController;
 import com.bu.haw.vier.data.dataObject;
 import com.bu.haw.vier.util.SystemUiHider;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -30,41 +28,6 @@ import java.util.ArrayList;
  */
 public class StartActivity extends Activity implements SensorEventListener{
 
-    /**
-     * todo: delete this shit until here
-     */
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
-
-    /**
-     * todo: delete this shit until here
-     */
-
     private SensorManager sensorManager;
     private MediaPlayer mediaPlayer;
     private ArrayList beschleunigungsDaten;
@@ -72,26 +35,39 @@ public class StartActivity extends Activity implements SensorEventListener{
 
     private double ax,ay,az;   // these are the acceleration in x,y and z axis
     private double max,min;
-    private Button dataStoreBtn;
 
+    private Button dataStoreBtn;
     TextView textView;
+
+    DataStorage dataStorage;
+    ExStorageController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        max = 0;
-        min = 100;
-
         setContentView(R.layout.activity_start);
-        startRecording = false;
-        beschleunigungsDaten = new ArrayList();
 
         sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
+
+        initObjects();
+        addListeners();
+    }
+
+    private void initObjects() {
+        max = 0;
+        min = 100;
+        startRecording = false;
+        beschleunigungsDaten = new ArrayList();
         textView = (TextView) findViewById(R.id.example);
         dataStoreBtn = (Button) findViewById(R.id.dummy_button);
 
+        controller = new ExStorageController(this, false);
+        dataStorage = new DataStorage(controller.getFile("save.txt"));
+
+    }
+
+    private void addListeners() {
         dataStoreBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -102,82 +78,12 @@ public class StartActivity extends Activity implements SensorEventListener{
                 } else {
                     startRecording = false;
                     dataStoreBtn.setText("start");
+                    dataStorage.writeToFile(beschleunigungsDaten);
                 }
 
             }
         });
-
-        /**
-         * todo: delete this shit
-         */
-
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
-        final View contentView = findViewById(R.id.fullscreen_content);
-
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
-
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.ur
-                            if (mControlsHeight == 0) {
-                                mControlsHeight = controlsView.getHeight();
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-                            controlsView.animate()
-                                    .translationY(visible ? 0 : mControlsHeight)
-                                    .setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                        }
-
-                        if (visible) {
-                            // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });
-
-        // Set up the user interaction to manually show or hide the system UI.
-        contentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
-                }
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-
-        /**
-         * todo: delete this shit until here
-         */
     }
-
 
 
     @Override
@@ -219,49 +125,5 @@ public class StartActivity extends Activity implements SensorEventListener{
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
     }
-
-    /**
-     * todo: delete this shit
-     */
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-        }
-    };
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
-    /**
-     * todo: delete this shit until here
-     */
 }
